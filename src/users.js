f1d8ec8bc5b89bf8900cd9d975f079ebd2122e27
Rituraj Ranjan <rituraj.ranjan.42@gmail.com>
2021-04-15 22:30:05 +0530

    3 users added

--- src/users.js
+++ src/users.js
@@ -13,6 +13,27 @@ var Users = [
 		rank: 2,
 		total: 2,
 	},
+	{
+		username: "minu",
+		password: "123",
+		attempted: 4,
+		rank: 2,
+		total: 2,
+	},
+	{
+		username: "khushboo",
+		password: "123",
+		attempted: 0,
+		rank: 0,
+		total: 0,
+	},
+	{
+		username: "navneet",
+		password: "123",
+		attempted: 0,
+		rank: 0,
+		total: 0,
+	},
 ];
 
 export default Users;
